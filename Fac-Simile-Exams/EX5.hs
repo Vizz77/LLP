ca251a368800628d5976cf4c03087c313091e98e
Vizz-VS <v.pregadio.vp@gmail.com>
2026-01-18 07:25:02 +0100

    Exercise Pattern matching

--- Fac-Simile-Exams/EX5.hs
+++ Fac-Simile-Exams/EX5.hs
@@ -6,3 +6,20 @@ posizione_dispari_lib xs = map snd (filter (\(i, _) -> odd i) (zip [0..] xs))
     where 
         odd :: Int -> Bool
         odd x = x `mod` 2 /= 0
+
+posizione_dispari_ric :: [a] -> [a]
+-- CASO 1: Lista con almeno 2 elementi (primo e secondo)
+-- _ (underscore) è il primo elemento (Indice 0, PARI) -> LO BUTTIAMO
+-- x è il secondo elemento (Indice 1, DISPARI) -> LO TENIAMO
+-- xs è tutto il resto della lista (dal 2 in poi)
+posizione_dispari_ric (_ : x : xs) = x : posizione_dispari_ric xs
+
+-- CASO 2: Qualsiasi altra cosa (Lista vuota o con 1 solo elemento)
+-- Se rimane 1 solo elemento, è in posizione pari (0), quindi lo buttiamo.
+posizione_dispari_ric _ = []
+
+scartaUnoPrendiDue :: [a] -> [a]
+-- Pattern match su almeno 3 elementi
+scartaUnoPrendiDue (_ : x : y : xs) = x : y : scartaUnoPrendiDue xs
+-- Caso base: se rimangono meno di 3 elementi, ci fermiamo
+scartaUnoPrendiDue _ = []
